1aa0a986c06660e3d4a2c33f32ef848c5d636e10
Charles Dobbs <carrollchar9@gmail.com>
2026-02-18 00:10:45 -0500

    third

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+     let total = 0;
+
+        cart.forEach((item) => {
+            const itemCost = parseFloat(item.cost.substring(1));
+            total += itemCost * item.quantity;
+        });
+
+        return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
-
+  
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
 
   const handleIncrement = (item) => {
+     
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+     dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let total = 0;
+
+    const itemCost = parseFloat(item.cost.substring(1));
+    total = item.quantity * itemCost;
+
+        return totalCost;
   };
 
   return (
